a3b3add25a01333d0440e2302138ceeff07beb20
facunav <facunav@yahoo.com.ar>
2025-07-21 21:27:42 -0300

    Se expuso explicitamente el puerto 8080

--- Sender.API/Program.cs
+++ Sender.API/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://*:8080");
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
